4a6e9fdf7a1a4e25804abcd075bf15792f380fd8
SvinotaKiller <pipidastrov1@mail.ru>
2025-03-28 12:42:02 +0300

    Две функции: первый коммит
    Были созданы две функции. Первая проверят наличие заданного файла в папке Catalogs (в котором будут храниться все каталоги созданные пользователем). Вторая функция создает сам каталог в папке Catalogs, для второй функции была задействована первая.

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+
+#Проверка на наличие файла в папке Catalogs
+def check_file_in_Catalogs(filename):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    catalogs_directory = os.path.join(current_directory, "Catalogs")
+    full_file_path = os.path.join(catalogs_directory, filename  + ".txt")
+    return os.path.isfile(full_file_path)
+
+#Создание каталога пользователем
+def create_catalog():
+    while True:
+        title = input("Введите название для вашего каталога: ")
+
+        if not check_file_in_Catalogs(title):
+            now = datetime.datetime.now()
+            date_string = now.strftime("%d-%m-%Y")
+
+            #Определяет где находится main.py; находит путь к папке Catalogs; создает полный путь к файлу внутри Catalogs
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            catalogs_path = os.path.join(current_dir, "Catalogs")
+            filepath = os.path.join(catalogs_path, f"{title}.txt")
+
+            file = open(filepath, "w", encoding="utf-8")
+            file.write(f"0\n{date_string}")
+            file.close()
+
+            print(f"Каталог с названием «{title}» успешно создан!")
+
+            break
+        else:
+            print(f"Каталог с названием {title} уже существует, попробуйте ещё раз.")
+
+create_catalog()
